ba4685b9a30803cfdc80a5e6fdb410936b848fd1
Sumeet Agarwal <sumeet.a@gmail.com>
2018-03-08 01:50:10 +0900

    serialization works on both platforms

--- local_clipboard.py
+++ local_clipboard.py
@@ -1,5 +1,9 @@
 from PyQt5.QtCore import QMimeData
+from PyQt5.QtCore import QByteArray
+from PyQt5.QtCore import QBuffer
+from PyQt5.QtCore import QIODevice
 from PyQt5.QtWidgets import QApplication
+
 
 class LocalClipboard(object):
 
@@ -32,8 +36,20 @@ class QMimeDataSerializer(object):
 
     @classmethod
     def serialize(cls, qmimedata):
-        formats = qmimedata.formats()
-        return {format: qmimedata.data(format).data() for format in formats}
+        # the normal case
+        if not qmimedata.hasImage():
+            formats = qmimedata.formats()
+            return {format: qmimedata.data(format).data() for format in formats}
+
+        # we have to special case images for two reasons:
+        # (1) the above serialization doesn't work on mac
+        # (2) on linux, the same image is available in many different formats.
+        #     if we were to use them all, we would send over the image several
+        #     times instead of just once
+
+        # TODO: see if this can be something other than png. i think it'll suck
+        # to send huge jpegs over the wire as png
+        return {'image/png': cls._extract_image(qmimedata)}
 
     @classmethod
     def deserialize(cls, serialized):
@@ -42,12 +58,20 @@ class QMimeDataSerializer(object):
             qmimedata.setData(format, data)
         return qmimedata
 
+    @classmethod
+    def _extract_image(cls, qmimedata):
+        ba = QByteArray()
+        buffer = QBuffer(ba)
+        buffer.open(QIODevice.WriteOnly)
+        qmimedata.imageData().save(buffer, 'PNG')
+        return ba.data()
+
 
 if __name__ == '__main__':
     app = QApplication([])
     clipboard = app.clipboard()
-    md = clipboard.mimeData()
 
+    md = clipboard.mimeData()
     print("serialized:")
     serialized = QMimeDataSerializer.serialize(md)
     print(serialized)
