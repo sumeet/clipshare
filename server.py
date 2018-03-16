d95fe1ac21684a0a505687a9a5580bf1434a6992
Sumeet Agarwal <sumeet.a@gmail.com>
2018-03-16 18:27:36 +0900

    maybe this'll run on dokku

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import websockets
 
 from coordinator import ClipboardsCoordinator
@@ -9,6 +10,7 @@ if __name__ == '__main__':
     coordinator = ClipboardsCoordinator()
     websocket_handler = WebsocketHandler(coordinator)
 
-    start_server = websockets.serve(websocket_handler.handle, '0.0.0.0', 8392)
+    port = os.environ['PORT']
+    start_server = websockets.serve(websocket_handler.handle, '0.0.0.0', port)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
