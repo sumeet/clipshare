92b8814e9de85620115d5fb8e0313f87be997293
Sumeet Agarwal <sumeet.a@gmail.com>
2018-03-25 17:28:25 -0700

    client will reconnect if it gets disconnected from server

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import threading
+import time
 
 import websockets
 
@@ -10,15 +11,16 @@ from websocket import MAX_PAYLOAD_SIZE
 from websocket import WebsocketHandler
 
 
+RECONNECT_WAIT_SECONDS = 5
 WS_URL = os.environ['WS_URL']
 
 
 async def client(websocket_handler, url):
     async with websockets.connect(url,  max_size=MAX_PAYLOAD_SIZE) as websocket:
-        await websocket_handler.handle(websocket, "the path is ignored anyway")
+        await websocket_handler.handle(websocket, 'the path is ignored anyway')
 
 
-if __name__ == '__main__':
+def start_client():
     coordinator = ClipboardsCoordinator()
 
     local_clipboard = LocalClipboard.new()
@@ -29,3 +31,14 @@ if __name__ == '__main__':
 
     asyncio.set_event_loop(local_clipboard.event_loop)
     local_clipboard.event_loop.run_until_complete(c)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            start_client()
+        except Exception as e:
+            print('got disconnected from the server')
+            print(e)
+            print(f'waiting {RECONNECT_WAIT_SECONDS} before reconnecting')
+            time.sleep(RECONNECT_WAIT_SECONDS)
