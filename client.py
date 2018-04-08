a6a54903072a972c84ebf3658deda956b2690499
Sumeet Agarwal <sumeet.a@gmail.com>
2018-04-08 02:53:50 -0700

    add a keepalive to fix connection issues

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from asyncio import ensure_future
 import asyncio
 import os
 import threading
@@ -14,9 +15,22 @@ from websocket import WebsocketHandler
 RECONNECT_WAIT_SECONDS = 5
 WS_URL = os.environ['WS_URL']
 
+KEEPALIVE_INTERVAL_SECONDS = 30
+
+
+# when running clipshare behind nginx, the server and client would seem to get
+# disconnected frequently. at least on dokku's settings. add thing keepalive
+# fixes the problem there. it's probably good to keep this in here. i bet it'll
+# help with other configurations as well.
+async def keepalive_forever(websocket, interval_seconds):
+    while True:
+        await asyncio.sleep(interval_seconds)
+        await websocket.ping()
+
 
 async def client(websocket_handler, url):
     async with websockets.connect(url,  max_size=MAX_PAYLOAD_SIZE) as websocket:
+        ensure_future(keepalive_forever(websocket, KEEPALIVE_INTERVAL_SECONDS))
         await websocket_handler.handle(websocket, 'the path is ignored anyway')
 
 
