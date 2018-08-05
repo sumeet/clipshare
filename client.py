4a1979d4632fd53b6bdaf159202ac2c92bfa0f50
Sumeet Agarwal <sumeet.a@gmail.com>
2018-08-05 17:34:57 +0900

    refactor connection initialization, think this will let me add pausing

--- client.py
+++ client.py
@@ -43,11 +43,65 @@ async def client(websocket_handler, url):
         await websocket_handler.handle(websocket, 'the path is ignored anyway')
 
 
-def start_client(qapp):
+class Connection:
+
+    def __init__(self, relay, ws_url):
+        self._websocket_handler = WebsocketHandler(relay)
+        self._ws_url = ws_url
+        self._client_future = None
+
+    def connect(self):
+        if self._is_connection_active:
+            raise Exception('fatal error: client running, but tried to start')
+
+        c = client(self._websocket_handler, self._ws_url)
+        self._client_future = ensure_future(c)
+        self._client_future.add_done_callback(self._client_future_done)
+
+    def disconnect(self):
+        if self._is_connection_active:
+            self._client_future.cancel()
+
+    # XXX: this doesn't neccessarily mean we're connected to the server. it
+    # could mean we're still trying to establish a connection
+    @property
+    def _is_connection_active(self):
+        return self._client_future and not self._client_future.done()
+
+    def _client_future_done(self, client_future):
+        if client_future.cancelled():
+            # if cancelled, that means WE cancelled it. don't reconnect
+            return
+        logger.info('got disconnected from the server')
+        e = client_future.exception()
+        if e:
+            logger.exception(e)
+        ensure_future(self._wait_and_reconnect())
+
+    async def _wait_and_reconnect(self):
+        logger.info(f'waiting {RECONNECT_WAIT_SECONDS} before reconnecting')
+        await asyncio.sleep(RECONNECT_WAIT_SECONDS)
+        self.connect()
+
+
+def start_ui(qapp):
+    ui = UI(qapp)
+    signals.incoming_transfer.connect(ui.handle_incoming_transfer_progress)
+    signals.outgoing_transfer.connect(ui.handle_outgoing_transfer_progress)
+    ui.start()
+    return ui
+
+
+if __name__ == '__main__':
+    qapp = QApplication([])
+
     # do this first because the rest of the proggy depnds on this being established
     # as the event loop
     event_loop = QEventLoop(qapp)
     asyncio.set_event_loop(event_loop)
+
+    # we need this to make it so ^c will quit the program
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     relay = Relay()
 
@@ -56,29 +110,10 @@ def start_client(qapp):
 
     websocket_handler = WebsocketHandler(relay)
 
-    ui = UI(qapp)
-    signals.incoming_transfer.connect(ui.handle_incoming_transfer_progress)
-    signals.outgoing_transfer.connect(ui.handle_outgoing_transfer_progress)
-    ui.start()
+    ui = start_ui(qapp)
 
-    c = client(websocket_handler, WS_URL)
+    connection = Connection(relay, WS_URL)
+    connection.connect()
 
     with event_loop:
-        event_loop.run_until_complete(c)
-
-
-if __name__ == '__main__':
-    # we need this to make it so ^c will quit the program
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-    while True:
-        try:
-            qapp = QApplication([])
-            start_client(qapp)
-        except Exception as e:
-            logger.info('got disconnected from the server')
-            logger.exception(e)
-            logger.info(f'waiting {RECONNECT_WAIT_SECONDS} before reconnecting')
-            time.sleep(RECONNECT_WAIT_SECONDS)
-        finally:
-            qapp.quit()
+        event_loop.run_forever()
